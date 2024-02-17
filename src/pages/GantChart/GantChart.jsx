a6b4b5330b12ef420331e65229a54fe5239c53fa
Abel <abelrula716@gmail.com>
2024-02-17 11:47:28 +0300

    added gant chart  for tracking projects in GantChart page

--- src/pages/GantChart/GantChart.jsx
+++ src/pages/GantChart/GantChart.jsx
@@ -1,8 +1,12 @@
- import "./gantChart.css"
+ import GantChartComponenet from "../../Componentes/GantChartComponenet/GantChartComponenet"
+import Header from "../../Componentes/Header/Header"
+import "./gantChart.css"
 const GantChart = () => {
   return (
-    <div>GantChart</div>
-  )
+   <div>
+      <Header title="Team" />
+      <GantChartComponenet />
+    </div>  )
 }
 
 export default GantChart
