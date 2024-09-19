1bb14c7587b412dac590d7bf5bea582e60553523
Abel Zewdu <abelrula716@gmail.com>
2024-09-19 05:28:14 -0900

    Implement user profile UI and made some adding in previewFile close tag and css styling

--- src/Componentes/PreviewFile/PreviewFile.jsx
+++ src/Componentes/PreviewFile/PreviewFile.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import { IoMdClose } from 'react-icons/io'
 import  "./previewFile.css"
+import { GrDocumentPdf } from 'react-icons/gr'
 
-const PreviewFile = ({fileNames}) => {
+const PreviewFile = ({fileNames,setAttachedDocuments}) => {
+ 
+ 
   return (
     <div className="Form__AttachDocuments-selectedFiles element-with-scroll">
       {/* if therse selected files map through them */}
       { fileNames?.map( ( ( fileName ) => (
-        <div className="Form__AttachDocuments-selectedFiles-file"><span>{ fileName }  <GrDocumentPdf className="icon" /></span>
+        <div className="Form__AttachDocuments-selectedFiles-file">
+          <span>{ `${ fileName.substring( 0, 26 ) }...` }  <GrDocumentPdf className="icon" /></span>
           {/* onClick on close the file icon delete selected files  */}
-                <IoMdClose className="closeIcon" onClick={()=>setAttachedDocuments(Array.from(attachedDocuments).filter(file=>file.name !== fileName))}/></div>
+          <IoMdClose
+            className="closeIcon"
+            onClick={ () => setAttachedDocuments( ( prev ) =>
+                 Array.from( prev )
+                .filter( file => file.name !== fileName ) ) } />
+            </div>
             ) ) ) }
           </div>
   )
