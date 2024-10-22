c1f3fcb11dde4a56e6dd55d5394cecf64e937472
Abel Zewdu <abelrula716@gmail.com>
2024-10-22 00:29:17 -0900

    Implement profile modal on teamMembers click on dashboard

--- src/Componentes/Dashboard/teamMembers/TeamMembers.jsx
+++ src/Componentes/Dashboard/teamMembers/TeamMembers.jsx
@@ -3,8 +3,13 @@ import { RiTeamLine } from "react-icons/ri";
 import "./teamMembers.css";
 import BoxHeader from "../../boxHeader/BoxHeader";
 import { PiDotsSixVerticalBold } from "react-icons/pi";
+import { useDispatch, useSelector } from "react-redux";
+import { openModal } from "../../../redux/slices/modalSlice";
+import ProfileModal from "../../Modals/ProfileModal/ProfileModal";
 const TeamMembers = () => {
+  const dispatch = useDispatch()
   const members = "http://localhost:3500/members";
+  const {modalType,toggled}=useSelector(state=>state.modal)
   const [member, setMember] = useState([]);
   useEffect(() => {
     async function fetchMembers() {
@@ -14,14 +19,17 @@ const TeamMembers = () => {
     }
     fetchMembers();
   }, []);
+  const Onclick =  () => dispatch( openModal( { modalType: "memberProfile", toggled: true } ) ) 
+
   return (
+   <>
     <div className="TeamDirectory">
  
       <BoxHeader icon={<RiTeamLine />} header="Team Directories"  />
       
       <div className="TeamDirectory__members element-with-scroll">
         {member?.map((member, i) => (
-          <div className="TeamDirectory__members__member" key={i}>
+          <div onClick={Onclick} className="TeamDirectory__members__member" key={i}>
             <img src={member.profile} alt="team-directory" />
             <div className="TeamDirectory__members__member--description">
               <h3>{member.name}</h3>
@@ -31,6 +39,11 @@ const TeamMembers = () => {
         ))}
       </div>
     </div>
+     { modalType === "memberProfile" && toggled === true &&
+          <ProfileModal />
+      }
+    </>
+    
   );
 };
 
--- src/Componentes/Modals/ProfileModal/ProfileModal.jsx
+++ src/Componentes/Modals/ProfileModal/ProfileModal.jsx
@@ -7,13 +7,21 @@ import { FaGraduationCap } from 'react-icons/fa6'
 import img1 from "../../../assets/worker2.jpg"
 import { MdOutlineWorkOutline } from 'react-icons/md'
 import { closeModal } from '../../../redux/slices/modalSlice'
-import { useDispatch } from 'react-redux'
-const ProfileModal = () => {
-  const dispatch = useDispatch()
+import { useDispatch, useSelector } from 'react-redux'
+import { LiaProjectDiagramSolid } from "react-icons/lia";
+import { FcTodoList } from "react-icons/fc";
 
+
+const ProfileModal = ({data}) => {
+  const dispatch = useDispatch()
+  const {modalType,toggled}=useSelector(state=>state.modal)
+ 
     return (
     <div className="modal">
-        <div className="profile_description">
+        <div className={
+          modalType === "memberProfile" &&
+          toggled === true ?
+          "memberProfile_description" : "profile_description" }>
           <div className="profile_description-header">
           <span className="signout"><CiLogout /> sign out </span>
            <IoCloseCircleSharp
@@ -30,42 +38,30 @@ const ProfileModal = () => {
                 <p>My account : my portal</p>
                </span>
           </div>
-        {/* <div className="profile_description__middle">
-            <div className="profile_description__middle-item">
-              <label>Bio :</label>
-              <p>livng with out pain is not the fact that we are alive</p>
-         </div>
-            <div className="profile_description__middle-item">
-              <label>Total friends :</label>
-              <span>120</span>
-         </div>
-          <div className="profile_description__middle-item">
-              <label>Accomplished Tasks :- </label>
-              <span>200</span>
-            </div>
-              <div className="profile_description__middle-item">
-              <label>Included Projects </label>
-                <ul>
-              <li>Operations</li>
-              <li>data managements</li>
-              <li>Networking</li>
-              </ul>
-         </div>
-          </div> */}
+       
              <div className="userInfo">
+            { modalType === "memberProfile" &&
+              <div className="profile_description__middle">
+               <p className="userInfo__list">
+                <span><FcTodoList />Accomplished Tasks :- </span>
+                  200 
+              </p>
+               <p className="userInfo__list">
+              <span><LiaProjectDiagramSolid />Included Projects </span>
+              <ul>
+                 <li>Operations</li>
+                 <li>data managements</li>
+                 <li>Networking</li>
+              </ul>
+         </p>
+          </div> 
+         }
             <p className="userInfo__list"><span><BiLogoOkRu /> Bio</span>The best and most beatiful things in the world cannot be ssen or even touched</p>
             <p className="userInfo__list"><span><CiLocationOn />Living in</span>   Austin ,Texas</p>
             <p className="userInfo__list"><span><FaGraduationCap />Went to</span>  the uniceresity of texas</p>
             <p className="userInfo__list"><span><MdOutlineWorkOutline />Works at</span> Atlas Mesa Solar</p>
+          <p className="userInfo__list"><span><BiCalendar />Joined In</span> June 17,2024</p>
             <button className="userInfo-Message__button" >Message</button>
-        <p className="userInfo__list"><span><BiCalendar />Joined In</span> June 17,2024</p>
-        {/* <div className="flex flex-wrap gap-1 mt-4 mx-auto">
-                {
-                  imagesPostedByTheOwner.map((image,i)=>(
-                    <img className="w-36 h-32 rounded-lg object-cover object-center" src={image} key={i} alt="posts" />
-                  ))
-                }
-              </div> */}
             </div>
          </div>
         </div>
